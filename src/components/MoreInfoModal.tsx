2dc540d19970f4111a3a6b71c1af832c77bbfb17
kssampson <kylesampsonmusic@gmail.com>
2024-09-30 12:43:35 -0700

    changed static content structure in LandingInfoCard and added a modal with MoreInfoModal. inport react-icons and chakraui icons

---
+++ src/components/MoreInfoModal.tsx
@@ -0,0 +1,77 @@
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, Text, Box, Heading, UnorderedList, ListItem, OrderedList, VStack, HStack } from "@chakra-ui/react";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { CgArrowLeft } from "react-icons/cg";
+import { CheckIcon } from '@chakra-ui/icons';
+
+
+const MoreInfoModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button variant="outline" colorScheme="blue" onClick={onOpen}>
+        I need more info first
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="lg">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Danielle's Birthday Book Surprise</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={6} align="left">
+              <Text fontSize="md">
+                We’re creating a high-quality printed memory book for Danielle. Friends and family can upload letters and photos. As Danielle goes through the book, she’ll see the photos on the left and the heartfelt notes, letters, or messages on the right.
+              </Text>
+
+              <Box>
+                <Heading size="sm" mb={3}>
+                  Ways to Participate:
+                </Heading>
+                <OrderedList spacing={3}>
+                  <HStack>
+                    <HStack w={"full"} justifyContent={"space-between"}>
+                      <ListItem>Create a secure account to:</ListItem>
+                      <HStack>
+                        <CheckIcon boxSize={"1em"} color={"green"}/>
+                        <Text pr={"1"}>Easy</Text>
+                        <CheckIcon boxSize={"1em"} color={"green"}/>
+                        <Text>Preferred</Text>
+                      </HStack>
+                    </HStack>
+                  </HStack>
+                  <UnorderedList pl={5}>
+                    <ListItem>Upload or take photo(s)</ListItem>
+                    <ListItem>Write letter in editor or upload</ListItem>
+                    <ListItem>Choice to make submission visible to others</ListItem>
+                    <ListItem>See the digital photobook!</ListItem>
+                  </UnorderedList>
+                  <ListItem>Submit via social media groups:</ListItem>
+                  <HStack fontSize={"xx-large"}>
+                    <Box>
+                      <FaFacebookSquare />
+                    </Box>
+                      <FaInstagram />
+                    <Box className="arrow-icon" pt={2}>
+                      <CgArrowLeft />
+                    </Box>
+                  </HStack>
+                </OrderedList>
+              </Box>
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Sign Up!</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default MoreInfoModal;
--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,7 +1,163 @@
+import { validateInputs } from "../utils/validateInputs";
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, Stack, VStack, useToast, Spinner, useDisclosure } from "@chakra-ui/react"
+import login from "../utils/login";
+import { redirect } from "react-router-dom";
+import { useEffect, useState } from "react";
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+import { useNavigate } from "react-router-dom";
+import OtpModal from "../components/OtpModal";
 
 
 const LogIn = () => {
 
+  const toast = useToast();
+
+  const [visitorId, setVisitorId] = useState<string | null>(null);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [usernameSubmitted, setUsernameSubmitted] = useState<boolean>(false);
+  const [passwordSubmitted, setPasswordSubmitted] = useState<boolean>(false);
+
+  const isErrorUsername = !validateInputs.isValidUsername(username) && usernameSubmitted;
+  const isErrorPassword = !validateInputs.isValidPassword(password) && passwordSubmitted;
+
+  const onChangeName = (e: any) => {
+    setUsernameSubmitted(false);
+    setUsername(e.target.value);
+  }
+
+  const onChangePassword = (e: any) => {
+    setPasswordSubmitted(false)
+    setPassword(e.target.value);
+  }
+
+  const resetUsernamePasswordStates = () => {
+    setUsername("");
+    setPassword("");
+    setUsernameSubmitted(false);
+    setPasswordSubmitted(false);
+    return null;
+  }
+
+  const onSubmit = async () => {
+      try {
+        setUsernameSubmitted(true);
+        setPasswordSubmitted(true);
+        const response = (await login(username, password, visitorId)).data;
+        if (response.success) {
+          const token = response.data.token;
+          const id = response.data.id;
+          localStorage.setItem("token", token);
+          resetUsernamePasswordStates();
+          setIsLoggedIn(true);
+          toast({
+            title: "Login successful.",
+            position: "top-right",
+            description: `${response.message}`,
+            status: "success",
+            duration: 7000,
+            isClosable: true,
+          });
+          resetUsernamePasswordStates();
+          redirect('/temp')
+        } else if (!response.success) {
+          //check for invalid email
+          if (response.invalidUsername || response.invalidPassword || response.noVisitorRecord) {
+            toast({
+              title: "Error:",
+              position: "top-right",
+              description: `${response.message}`,
+              status: "error",
+              duration: 7000,
+              isClosable: true,
+            });
+            resetUsernamePasswordStates();
+          } else if (response.needs2Fa) {
+            toast({
+              title: "Notice: ",
+              position: "top-right",
+              description: `${response.message}`,
+              status: "error",
+              duration: 7000,
+              isClosable: true,
+            });
+            onOpen();
+          }
+        }
+        return;
+      } catch (error) {
+        toast({
+          title: "Error logging in. Please try again.",
+          position: "top-right",
+          description: `${error}`,
+          status: "error",
+          duration: 7000,
+          isClosable: true,
+        });
+        resetUsernamePasswordStates();
+      }
+  }
+
+  useEffect(() => {
+    const setFp = async () => {
+      const fp = await FingerprintJS.load();
+      const { visitorId } = await fp.get();
+      setVisitorId(visitorId);
+    };
+
+    setFp();
+  }, []);
+
+  return (
+    <Box>
+      <VStack >
+        <Heading mb={6}>Log-In</Heading>
+        <Box maxWidth={"75%"} width={"100%"}>
+          <Stack spacing={3}>
+                <Box>
+                  <FormControl isInvalid={isErrorUsername} isRequired>
+                    <FormLabel>Username:</FormLabel>
+                    <Input type='text' value={username ? username : ""} onChange={onChangeName} />
+                    {!isErrorUsername ? null : (
+                      <FormErrorMessage>Name is required.</FormErrorMessage>
+                    )}
+                  </FormControl>
+                </Box>
+                <Box>
+                  <FormControl isInvalid={isErrorPassword} isRequired>
+                    <FormLabel>Password:</FormLabel>
+                    <Input type='password' value={password} onChange={onChangePassword} />
+                    {!isErrorPassword ? null : (
+                      <FormErrorMessage>Password is required.</FormErrorMessage>
+                    )}
+                  </FormControl>
+                </Box>
+                <Button
+                colorScheme='blue'
+                onClick={onSubmit}
+                >Submit
+                </Button>
+          </Stack>
+        </Box>
+      </VStack>
+      <OtpModal
+          visitorId={visitorId}
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+          isOpen={isOpen}
+          onOpen={onOpen}
+          onClose={onClose}
+          setIsLoggedIn={setIsLoggedIn}
+        />
+    </Box>
+  )
 }
 
 export default LogIn;
